8eb3b7667ba4c99e5ce47aed88e6dbcc2e7dfe15
MidasNG <kyrdia2@alu.edu.gva.es>
2024-01-15 18:28:37 +0100

    Pausa arreglada

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 
-public class Pause : MonoBehaviour
+public class PauseTruck : MonoBehaviour
 {
     private bool pause = false;
     private GameObject continuar, salir, texto;
@@ -25,22 +25,24 @@ public class Pause : MonoBehaviour
         if (Input.GetButtonDown("Cancel"))
         {
             pause = !pause;
-        }
+            if (pause)
+            {
+                continuar.SetActive(true);
+                salir.SetActive(true);
+                texto.SetActive(true);
+                Time.timeScale = 0;
+                print("flipA");
+            }
+            else
+            {
+                continuar.SetActive(false);
+                salir.SetActive(false);
+                texto.SetActive(false);
+                Time.timeScale = 1;
+                print("flipB");
+            }
 
-        //Los ifs son para el minijuego truck
-        if (pause && Time.timeScale == 1)
-        {
-            continuar.SetActive(true);
-            salir.SetActive(true);
-            texto.SetActive(true);
-            Time.timeScale = 0;
-        }
-        else if (Time.timeScale == 0)
-        {
-            continuar.SetActive(false);
-            salir.SetActive(false);
-            texto.SetActive(false);
-            Time.timeScale = 1;           
+        
         }
     }
 
