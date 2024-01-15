8f55bcf0b7d495a8f6f64fab24d6ca33903e64bd
Alecatlop <alecatlop@alu.edu.gva.es>
2024-01-15 19:34:07 +0100

    pantallas carga

--- Assets/Scripts/Interfaz/modo controles.cs
+++ Assets/Scripts/Interfaz/modo controles.cs
@@ -8,21 +8,22 @@ using UnityEngine.SceneManagement;
 public class modocontroles : MonoBehaviour
 {
     public GameObject aviso;
+    public GameObject controles;
     private bool colider = false;
     
 
     void Start()
     {
         aviso.SetActive(false);
-        
+        controles.SetActive(false);
+
     }
 
     private void Update()
     {
         if (Input.GetButtonDown("Interact") && colider == true)
         {
-          
-
+            controles.SetActive(true);
         }
     }
 
